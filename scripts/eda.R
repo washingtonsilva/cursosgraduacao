13f363791ff418f23e0822840840567bf1f64abe
washingtonsilva <washington.silva@ifmg.edu.br>
2018-09-11 00:29:44 -0300

    Alterando o conjunto de dados

--- scripts/eda.R
+++ scripts/eda.R
@@ -5,8 +5,10 @@ load("dados_grad_grad_misscomp.RData")
 # Pacotes que serao utilizados
 library(tidyverse)
 library(ggplot2)
+library(ggthemes)
 library(plotly)
 library(gganimate)
+library(showtext)
 
 
 # Selecionando CURSO via dplyr::filter()
@@ -17,18 +19,51 @@ Engenharia
 Matematica <- filter(dados_grad, curso == "MATEMÁTICA")
 Matematica 
 
-# ggplot2
+# ggplot2 e ggplotly
+
+#########################################################################
+##                    ENGENHARIA ELETRICA                              ##
+#########################################################################
 
 #### Matriculas Inicais
 p1 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = matriculas_iniciais, group = curso)) + geom_point() + geom_line() 
+
+p1 <- p1 + labs(title = "Matrículas Iniciais por Período Letivo",
+           subtitle = "Conceito: Total de alunos matriculados por período letivo") +
+                 labs(x = "Período Letivo", y = "Alunos Matriculados")
+
+# Theme: The Economist
+p1 <-  p1 + theme_economist() + theme(axis.line.x = element_line(size = .5, colour = "black"))
+p1
 ggplotly(p1)
 
-p1 <- ggplot(data = Matematica, aes(x = periodo_letivo, y = matriculas_iniciais, group = curso)) + geom_point() + geom_line() 
+# Theme: Five Thirty Eight
+p1 <-  p1 + theme_fivethirtyeight() + theme(axis.line.x = element_line(size = .5, colour = "black"))
+p1
 ggplotly(p1)
 
-#### Matriculas Inicais
-p2 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = cumsum(matriculas_iniciais), group = curso)) + geom_point() + geom_line() 
+# Theme: Wall Street Journal
+p1 <-  p1 + theme_wsj() + theme(axis.line.x = element_line(size = .5, colour = "black"))
+p1
+ggplotly(p1)
+
+## Concluintes
+p2 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = concluintes, group = curso)) + geom_point() + geom_line() 
 ggplotly(p2)
 
+## Trancamento
+p3 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = trancamento, group = curso)) + geom_point() + geom_line() 
+ggplotly(p3)
 
+## Retencao
+p4 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = retidos, group = curso)) + geom_point() + geom_line() 
+ggplotly(p4)
+
+## Evasao absoluta
+p5 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = evasao, group = curso)) + geom_point() + geom_line() 
+ggplotly(p5)
+
+## Taxa de Evasao 
+p6 <- ggplot(data = Engenharia, aes(x = periodo_letivo, y = taxa_evasao, group = curso)) + geom_point() + geom_line() 
+ggplotly(p6)
 
